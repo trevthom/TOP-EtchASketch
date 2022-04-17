648b5739fbe6fea866a7621078a8a2cd2555e2e6
Trevor Thompson <thompson.trevor@protonmail.com>
2022-04-17 19:44:48 -0400

    First iteration Etch-a-Sketch

--- scripts.js
+++ scripts.js
@@ -1,21 +1,47 @@
 
 //1335 x 520
 
-for (let i = 0; i < 16; i++) {
-    const divRow = document.createElement('div');
-    divRow.id = 'divRow' + i.toString();
-    divRow.style.display = 'flex';
-    document.body.appendChild(divRow);
-    console.log(divRow);
-    for (let j = 0; j < 16; j++) {
-        const divColumn = document.createElement('div');
-        divColumn.width = 1335 / j; //j depends on userinput
-        divColumn.height = 52 / j;
-        divColumn.addEventListener("mouseover", e => {
-            divColumn.style.backgroundColor = `rgba(0,0,0,1)`
-        })
-        document.getElementById('divRow' + i.toString()).appendChild(divColumn);
+let newGrid = document.getElementById('newGrid');
+let shade = 0.1;
+
+newGrid.addEventListener('click', e => {
+    let gridSize = '';
+    gridSize = prompt('How many rows/columns?');
+    createNewGrid(gridSize);
+    }
+);
+
+function createNewGrid(gridSize) {
+    document.querySelectorAll('.gridRow').forEach(e => e.remove());
+
+    //create grid rows (container divs for columns)
+    for (let i = 0; i < parseInt(gridSize); i++) {
+        const divRow = document.createElement('div');
+        divRow.className = 'gridRow';
+        document.getElementById('container').appendChild(divRow);
+        for (let j = 0; j < parseInt(gridSize); j++) {
+            const divColumn = document.createElement('div');
+            divRow.id = 'divRow' + i.toString();
+            divColumn.style.backgroundColor = 'rgba(0,0,0,0)';
+            divColumn.addEventListener("mouseover", e => {
+                e.target.style.backgroundColor = changeOpacity(e.target.style.backgroundColor);
+                
+            })
+            document.getElementById('divRow' + i.toString()).appendChild(divColumn);
+        }
     }
 }
 
-
+function changeOpacity(alpha) {
+    if (alpha.endsWith('.9)') || alpha.endsWith(', 1)')) {
+        return 'rgba(0, 0, 0, 1)';
+    } else if(parseInt(alpha.charAt(16)) < 9) {
+        let newAlpha = parseInt(alpha.charAt(16)) + 1;
+        return alpha.replace(alpha.charAt(16), newAlpha);
+    } else if (alpha.startsWith('rgb(')) {
+        return 'rbg(0, 0, 0)';
+    } else if (alpha.endsWith(', 0)')) {
+        console.log("here");
+        return 'rgba(0, 0, 0, 0.1)';
+    }  
+}
